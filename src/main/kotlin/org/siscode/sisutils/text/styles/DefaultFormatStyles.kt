0d0ae2d60607cd7d0178b7ef00b8ec4ffca6010d
roridev <prates.aliceic@gmail.com>
2022-01-15 06:49:15 -0300

    Add `DefaultFormatStyles.kt`

---
+++ src/main/kotlin/org/siscode/sisutils/text/styles/DefaultFormatStyles.kt
@@ -0,0 +1,18 @@
+package org.siscode.sisutils.text.styles
+
+import org.siscode.sisutils.text.FormatStyle
+
+object DefaultFormatStyles {
+    @JvmField
+    val BOLD = FormatStyle("BOLD")
+    @JvmField
+    val EMPTY = FormatStyle("EMPTY")
+    @JvmField
+    val ITALIC = FormatStyle("ITALIC")
+    @JvmField
+    val UNDERSCORE = FormatStyle("UNDERSCORE")
+    @JvmField
+    val STRIKETHROUGH = FormatStyle("STRIKETHROUGH")
+    @JvmField
+    val OBFUSCATED = FormatStyle("OBFUSCATED")
+}
