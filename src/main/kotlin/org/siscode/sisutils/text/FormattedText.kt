60ddcbe072d1cddfc3fbcf60177a9257a7bdc7c3
roridev <prates.aliceic@gmail.com>
2022-01-15 06:47:59 -0300

    Add `StyledText` class

--- src/main/kotlin/org/siscode/sisutils/text/FormattedText.kt
+++ src/main/kotlin/org/siscode/sisutils/text/FormattedText.kt
@@ -3,3 +3,4 @@ package org.siscode.sisutils.text
 sealed class FormattedText
 
 class LiteralText(val text: String) : FormattedText()
+class StyledText(val styles: FormatStyle, val children: List<FormattedText>) : FormattedText()
