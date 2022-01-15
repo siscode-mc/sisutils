5af5af9de3c3a1cda2aa6be6400ade52d20c9127
roridev <prates.aliceic@gmail.com>
2022-01-15 19:06:29 -0300

    Add `MojTextAdapter.kt`

---
+++ src/main/kotlin/org/siscode/sisutils/text/adapters/MojTextAdapter.kt
@@ -0,0 +1,15 @@
+package org.siscode.sisutils.text.adapters
+
+import net.minecraft.text.Text
+import org.siscode.sisutils.text.FormattedText
+import org.siscode.sisutils.text.TextAdapter
+
+class MojTextAdapter : TextAdapter<Text> {
+    override fun parse(obj: Text): FormattedText {
+        TODO("Not yet implemented")
+    }
+
+    override fun serialize(formattedText: FormattedText): Text {
+        TODO("Not yet implemented")
+    }
+}
